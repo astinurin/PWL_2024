ad14e7adf54414b196d7e1319eb634741080dd50
astinurin <astihidayanti20@gmail.com>
2024-02-29 00:06:14 +0700

    awal-resource controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return 'U need to insert the code first!';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,96 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+   });
+
+Route::get('/world', function () {
+    return 'World';
+});
+   
+Route::get('/txt', function () {
+    return 'Soobin ';
+});
+   
+Route::get('/welcome', function () {
+    return 'Selamat datang ';
+});
+
+Route::get('/about', function () {
+    return ' Nama   : Asti Nurin Hidayanti <br>  NIM    : 2241720236';
+});
+
+//Route Parameters :
+Route::get('/user/{Asti}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/user/{Soobin}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+
+    Route::get('/posts/{postId}/comments/{commentId}', function
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+    
+    
+    
+// Optional Parameters
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+}); //nanti bisa ubah atau beri nama di alamat atau url
+
+Route::get('txt/{cb?}', function ($cb=null){
+    return 'TXT COMEBACK IN '.$cb;
+});
+
+// Route::get('/user/{name?}', function ($name='John') {
+//     return 'Nama saya '.$name;
+// }); //ohh variabel namanya juga bisa langsung ditulis di sini
+Route::get('/user/{name?}', function ($name='Danielle') {
+    return 'Nama saya '.$name;
+}); 
+
+
+//ROUTE NAME 
+Route::get('/user/profile', function() {
+    //
+    })->name('profile');
+
+
+
+//CONTROLLER
+use App\Http\Controllers\WelcomeController;
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+
+use App\Http\Controllers\PageController;
+Route::get('/index', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+use App\Http\Controllers\HomeController;
+Route::get('/home', [HomeController::class, 'home']);
+
+use App\Http\Controllers\AboutController;
+Route::get('/about', [AboutController::class, 'about']);
+
+use App\Http\Controllers\ArticleController;
+Route::get('/article/{id}', [ArticleController::class, 'article'] );
+
+
+//RESOURCE CONTROLLER 
+use App\Http\Controllers\PhotoController;
+Route::resource('photos', PhotoController::class);
+
+// Route::resource('photos', PhotoController::class)->only([
+//     'index', 'show'
+//     ])//mereka berdua atas bawah dipake 1 aja gaksih
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+    ]);
